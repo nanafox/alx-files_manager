0d9ea64826e78d45e11babdbaa1f982a8cb5f6ee
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-08-30 17:23:33 +0000

    feat: Add endpoint for creating new users

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 import { Router } from 'express';
-import AppController from '../controllers/AppController';
+import { AppController, UsersController } from '../controllers/AppController';
 
 const router = Router();
 
 router.get('/status', async (req, res) => AppController.getStatus(req, res));
 router.get('/stats', async (req, res) => AppController.getStats(req, res));
+
+router.post('/users', async (req, res) => UsersController.postNew(req, res));
 
 export default router;
