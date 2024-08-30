03352d4e7df14cf2dac54dfb68c8430c4e8bf94b
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-08-30 21:49:53 +0000

    feat: Add a controller for retrieving user details

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,8 @@
+import { ObjectId } from 'mongodb';
 import { hashPassword } from '../utils/auth';
 
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   /**
@@ -34,6 +36,40 @@ class UsersController {
       return res.status(201).json({ id: newUser.insertedId, email });
     } catch (error) {
       return res.status(500).json({ error: 'An error occurred while creating new user' });
+    }
+  }
+
+  /**
+   * Retrieve the current user data based on the token.
+   * @param {Object} req - The request object.
+   * @returns {Object} The user data.
+   */
+  static async getUserData(req) {
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
+  }
+
+  /**
+   * Get the current user based on the token and send the response.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
+  static async getMe(req, res) {
+    try {
+      const user = await UsersController.getUserData(req);
+      return res.status(200).json({ id: user._id, email: user.email });
+    } catch (error) {
+      return res.status(401).json({ error: error.message });
     }
   }
 }
