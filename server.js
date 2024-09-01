494d8abfd52ba4c9d1fed0bd61aa83528ee89a48
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-09-01 00:06:43 +0000

    chore: Add health check middleware for API and database

--- server.js
+++ server.js
@@ -2,10 +2,26 @@
 
 import express from 'express';
 import indexRoutes from './routes/index';
+import dbClient from './utils/db';
+import HTTPError from './utils/httpErrors';
+import redisClient from './utils/redis';
 
 const api = express();
 const port = process.env.PORT || 5000;
 
+const checkApiAndDBHealth = async (req, res, next) => {
+  if (!(await dbClient.isAlive())) {
+    return HTTPError.internalServerError(res);
+  }
+
+  if (!redisClient.isAlive()) {
+    return HTTPError.internalServerError(res);
+  }
+
+  return next();
+};
+
+api.use(checkApiAndDBHealth);
 api.use(express.json());
 api.use('/', indexRoutes);
 
